7f3a5504243a2abe8de9011cb436d002df2775d5
mottox2 <y.mottox2@gmail.com>
2018-07-22 22:47:08 +0900

    Parse JSON

--- functions/slack.js
+++ functions/slack.js
@@ -1,6 +1,6 @@
 exports.handler = function (event, context, callback) {
   console.log(JSON.stringify(event.body))
-  const body = event.body
+  const body = JSON.parse(event.body)
   callback(null, {
     statusCode: 200,
     body: body.challenge
