683a02cafcd307867946d6683ac2d40dbfc46712
mottox2 <y.mottox2@gmail.com>
2018-07-22 22:44:55 +0900

    Accept url_verification event

--- functions/slack.js
+++ functions/slack.js
@@ -1,7 +1,8 @@
 exports.handler = function (event, context, callback) {
-  console.log(JSON.stringify(event))
+  console.log(JSON.stringify(event.body))
+  const body = event.body
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: body.challenge
   });
 }
